520c21b1cf74234f498a1e1b37b3feac8357cc2b
Jun Lim <junyuen@gmail.com>
2013-01-10 21:52:38 +0800

    Initial commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def host
+  end
+
+  def find_local
+  end
+
+  def locals
+  end
+
+  def thank_you
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hungryhippie::Application.config.secret_token = '47ff59ea58781c2d1b84bcc80eaf7363e84596b4788a0c9db5191c5d5aea08b6784e1ac651edecdaf886706361d7a0080fb3e97232684439a8f8aacca89f0a18'
