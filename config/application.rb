17a21d42ae6e217f4b0595fa6a4fa47cfb20e99d
Lim Jun Yuen <junyuen@gmail.com>
2013-02-03 11:53:07 +0800

    Precompiler solution?

--- config/application.rb
+++ config/application.rb
@@ -59,7 +59,7 @@ module Hungryhippie
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     
-    #Pre-compile solutions?
-    config.assets.initialize_on_precompile = false
+    # Pre-compile solutions?
+    # config.assets.initialize_on_precompile = false
   end
 end
