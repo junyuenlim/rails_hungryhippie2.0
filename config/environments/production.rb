1a77a5350204db82c8581ef0875508385ce0b8e6
Lim Jun Yuen <junyuen@gmail.com>
2013-02-03 15:24:23 +0800

    Added production ENV production as Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Hungryhippie::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-
+  config.action_mailer.default_url_options = { :host => 'http://serene-atoll-1160.herokuapp.com/' }
+  
   # Code is not reloaded between requests
   config.cache_classes = true
 
